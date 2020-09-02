229192531ccba6c5d90483295d82aca773ba41c9
Kunal Juneja <47492588+cashewit@users.noreply.github.com>
2020-09-02 16:40:07 +0530

    Assignment 4

---
+++ Operating Systems/assignment_4.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <pthread.h> 
+#include<sys/wait.h>
+ 
+void *myThread1(void *vargp) 
+{ 
+   int *myid = (int *)vargp;
+    printf("Thread 1 ID: %d\n", *myid); 
+ 
+} 
+ 
+void *myThread2(void *vargp) 
+{ 
+   int *myid = (int *)vargp;
+    printf("Thread 2 ID: %d\n", *myid); 
+ 
+} 
+ 
+int main() 
+{
+    pthread_t thread_1;  
+    pthread_t thread_2;
+
+    printf("Start of Process\n");
+    int pid = fork();
+
+    pthread_create(&thread_1, NULL, myThread1, (void *)&thread_1);
+    pthread_join(thread_1, NULL); 
+ 
+    if(pid==0)
+    {
+        printf(" In child process \n");   
+         
+        printf("pid=%d ,parent=%d\n",getpid(),getppid());
+        pthread_create(&thread_2, NULL, myThread2, (void *)&thread_2);
+        pthread_join(thread_2, NULL); 
+    }
+    else{
+        wait(NULL);
+        printf(" In Parent process\n");
+        printf("pid=%d ,parent=%d\n",getpid(),getppid());
+     
+        pthread_create(&thread_2, NULL, myThread2, (void *)&thread_2);
+        pthread_join(thread_2, NULL); 	
+ 
+   }
+ 
+}
